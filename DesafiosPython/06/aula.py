7956ededa057d9e62ecfebf3daa6a0badd56b52c
Gedasio Silva <Gedasio.pereira@hotmail.com>
2025-03-13 19:11:08 -0300

    Aprendendo o Decorador

--- DesafiosPython/06/aula.py
+++ DesafiosPython/06/aula.py
@@ -1,15 +1,22 @@
 # Exercício - Adiando execução de funções
-def soma(x, y):
+def soma(x, y): #criando a função soma
     return x + y
 
 
-def multiplica(x, y):
+def multiplica(x, y):#criando a função multiplica
     return x * y
 
 
-def criar_funcao(funcao, *args):
-    return funcao(*args)
+def criar_funcao(funcao,x):
+    def interna(y):
+        return funcao(x,y)
+    return interna
 
-
-soma_com_cinco = criar_funcao(soma, 5)
+#soma recebe criar um funcao que recebe a função soma
+soma_com_cinco = criar_funcao(soma, 5) 
+#multiplica_por_dez recebe criar um funcao que recebe a função multiplica
 multiplica_por_dez = criar_funcao(multiplica, 10)
+
+print(soma_com_cinco(10))
+print(multiplica_por_dez(10))
+
